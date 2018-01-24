58022de8c96b850b6a938ab28a09ca4ec941b155
Buglight <buglight@kistriver.com>
2018-01-24 16:49:51 +0300

    Stack implementation

---
+++ stack.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include "stack.h"
+
+int stack_push(stack_t *s, item_t value)
+{
+    if (s->top == STACK_SIZE)
+    {
+        return 1;
+    }
+    s->items[s->top] = value;
+    s->top++;
+    return 0;
+}
+
+int stack_pop(stack_t *s, item_t *value)
+{
+    if (s->top == 0)
+    {
+        return 1;
+    }
+    s->top--;
+    *value = s->items[s->top];
+    return 0;
+}
---
+++ stack.h
@@ -0,0 +1,22 @@
+#include "list.h"
+
+#ifndef STACK_H
+#define STACK_H
+
+#define STACK_SIZE 16
+
+typedef struct stack stack_t;
+
+struct stack
+{
+    int top;
+    item_t items[STACK_SIZE];
+};
+
+#define STACK_INIT() {.top = 0}
+
+int stack_push(stack_t *s, item_t value);
+
+int stack_pop(stack_t *s, item_t *value);
+
+#endif  // STACK_H
